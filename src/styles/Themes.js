b7059acf021c4ace2804d20322be2b1adba24191
LeezyLazyCrazy <leeg1dance@gmail.com>
2022-04-19 17:17:39 +0900

    first commit

---
+++ src/styles/Themes.js
@@ -0,0 +1,45 @@
+//  This  file contains variables for different theme
+
+export const light = {
+
+  body: "#fff",
+  text: "#202020", //black shade
+  bodyRgba: "255,255,255",
+  textRgba: "32,32,32",
+
+  carouselColor: '#eeedde',
+
+  fontxs: '0.75em',
+  fontsm: '0.875em',
+  fontmd: '1em', //1em=16px
+  fontlg: '2em',
+  fonttxl: '3em',
+  fontxxl: '4em',
+
+  fontButton:'0.875em',
+
+  navHeight: '5rem',
+}
+
+
+export const dark = {
+
+  body: "#202020",
+  text: "#fff", //black shade
+  bodyRgba: "32,32,32",
+  textRgba:  "255,255,255",
+
+  carouselColor: '#eeedde',
+
+  fontxs: '0.75em',
+  fontsm: '0.875em',
+  fontmd: '1em', //1em=16px
+  fontlg: '2em',
+  fonttxl: '3em',
+  fontxxl: '4em',
+
+  fontButton:'0.875em',
+
+  navHeight: '5rem',
+}
+
