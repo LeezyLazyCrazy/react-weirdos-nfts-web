a24b41029f1273a279c8ceb76b5460cd11eb582e
LeezyLazyCrazy <leeg1dance@gmail.com>
2022-04-20 19:46:50 +0900

    About section add

--- src/components/Button.js
+++ src/components/Button.js
@@ -39,8 +39,8 @@ const Btn = styled.button`
 const Button = ({text, link}) => {
   return (
     <Btn>
-      <a href={link} aria-label={text} target="__blank" rel='noreferrer'>
-        {text}
+      <a href={link} aria-label={text} target="_blank" rel="noreferrer" >
+      <Btn>{text}</Btn>
       </a>
     </Btn>
   )
