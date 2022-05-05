c574fc050745a77e6551b2689e790057f7169e06
LeezyLazyCrazy <leeg1dance@gmail.com>
2022-05-05 22:39:22 +0900

    checked all section

--- src/components/sections/About.js
+++ src/components/sections/About.js
@@ -1,154 +1,127 @@
 import React from 'react'
-import styled,{ThemeProvider} from 'styled-components'
+import styled,{ ThemeProvider }from 'styled-components'
 import Carousel from '../Carousel'
 import Button from '../Button'
-import {dark} from '../../styles/Themes' 
+import {dark} from '../../styles/Themes'
 
-const Section = styled.section `
-  min-height:100vh;
-  width:100%;
-  background-color: ${props => props.theme.text};
-  display:flex;
-  justify-content:center;
-  align-items: center;
-  position: relative;
+
+const Section = styled.section`
+min-height: 100vh;
+width: 100%;
+background-color: ${props => props.theme.text};
+display: flex;
+justify-content: center;
+align-items: center;
+position: relative;
 `
-
 const Container = styled.div`
-  width: 75%;
-  margin: 0 auto;
-  // background-color: lightblue;
-
-  display: flex;
-  justify-content: center;
-  align-items: center;
-
-  @media(max-width:70em){
-    width:85%;
+width: 75%;
+margin: 0 auto;
+display: flex;
+justify-content: center;
+align-items:center;
+@media(max-width: 70em){
+  width: 85%;
+}
+@media(max-width: 64em){
+  width: 100%;
+  flex-direction: column;
+  &>*:last-child{
+    width: 80%;
   }
-
-  @media(max-width:64em){
-    width:100%;
-    flex-direction: column;
-
-    &>*:last-child{
-      width:80%;
-    }
+}
+@media(max-width: 40em){
+  &>*:last-child{
+    width: 90%;
   }
-
-  @media(max-width:40em){
-    &>*:last-child{
-      width:90%;
-    }
+}
 `
-
 const Box = styled.div`
   width: 50%;
   height: 100%;
   min-height: 60vh;
   display: flex;
-  justify-content: column;
-  align-items: center;
   flex-direction: column;
-
-  
-  @media(max-width:40em){
-    min-height:50vh;
+  justify-content: center;
+  align-items: center;
+  @media(max-width: 40em){
+    min-height: 50vh;
   }
 `
-
 const Title = styled.h2`
   font-size: ${props => props.theme.fontxxl};
   text-transform: capitalize;
-  width: 80%;
-  color: ${props=> props.theme.body};
+  color: ${props => props.theme.body};
   align-self: flex-start;
-  margin:0 auto;
-
-  @media(max-width:64em){
-    width:100%;
-    text-align:center;
+  width: 80%;
+  margin: 0 auto;
+  @media(max-width: 64em){
+    width: 100%;
+    text-align: center;
   }
-
-  @media(max-width:40em){
+  @media(max-width: 40em){
     font-size: ${props => props.theme.fontxl};
   }
-
-  @media(max-width:30em){
-      font-size: ${props => props.theme.fontlg};
+  @media(max-width: 30em){
+    font-size: ${props => props.theme.fontlg};
   }
 `
-
 const SubText = styled.p`
-  font-size: ${props => props.theme.fontlg}; 
-  color: ${(props)=> `rgba(${ props.theme.bodyRgba},0.6)`};
+  font-size: ${props => props.theme.fontlg};
+  color: ${props => props.theme.body};
   align-self: flex-start;
   width: 80%;
   margin: 1rem auto;
-  font-weight:400;
-
-  @media(max-width:64em){
-    width:100%;
-    text-align:center;
+  font-weight: 400;
+  @media(max-width: 64em){
+    width: 100%;
+    text-align: center;
     font-size: ${props => props.theme.fontmd};
   }
-
-  @media(max-width:40em){
+  @media(max-width: 40em){
     font-size: ${props => props.theme.fontmd};
   }
-
-  @media(max-width:30em){
-      font-size: ${props => props.theme.fontsm};
+  @media(max-width: 30em){
+    font-size: ${props => props.theme.fontsm};
   }
 `
-
 const SubTextLight = styled.p`
-  font-size: ${props => props.theme.fontmd}; 
-  color: ${(props)=> `rgba(${ props.theme.bodyRgba},0.6)`};
+  font-size: ${props => props.theme.fontmd};
+  color: ${props => `rgba(${props.theme.bodyRgba},0.6)`};
   align-self: flex-start;
   width: 80%;
   margin: 1rem auto;
-  font-weight:400;
-
-  @media(max-width:64em){
-    width:100%;
-    text-align:center;
+  font-weight: 400;
+  @media(max-width: 64em){
+    width: 100%;
+    text-align: center;
     font-size: ${props => props.theme.fontsm};
   }
-
-  @media(max-width:40em){
+  @media(max-width: 40em){
     font-size: ${props => props.theme.fontsm};
   }
-
-  @media(max-width:30em){
-      font-size: ${props => props.theme.fontxs};
+  @media(max-width: 30em){
+    font-size: ${props => props.theme.fontxs};
   }
 `
-
-const ButtonContainer = styled.div `
-  width:80%;
-  display:flex;
+const ButtonContainer = styled.div`
+  width: 80%;
   margin: 1rem auto;
+  display: flex;
   align-self: flex-start;
-
-  
-  @media(max-width:64em){
-    width:100%;
-
+  @media(max-width: 64em){
+    width: 100%;
     button{
-      margin:0 auto;
+      margin: 0 auto;
     }
   }
 `
-
 const About = () => {
   return (
-    <Section id='about'>
+    <Section id="about">
       <Container>
-        <Box>
-          <Carousel/>
-        </Box>
-        <Box>
+        <Box> <Carousel /> </Box>
+        <Box> 
           <Title>
             Welcome To The <br/> Weirdos Club.
           </Title>
@@ -159,8 +132,8 @@ const About = () => {
             With more than 200+ hand drawn traits, each NFT is unique and comes with a membership to an exclusive group of successful investors. Join an ambitious ever-growing community with multiple benefits and utilities.
           </SubTextLight>
           <ButtonContainer>
-            <ThemeProvider theme={dark}>
-              <Button text="JOIN OUR DISCORD" Link="#"/>
+            <ThemeProvider theme={dark} >
+              <Button text="JOIN OUR DISCORD" link="#" />
             </ThemeProvider>
           </ButtonContainer>
         </Box>
